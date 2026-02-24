8357bf121538c396640c315671f3ce94e75f69b0
Olga Cantalejo <2004olgacg@gmail.com>
2026-02-24 13:30:22 +0100

    feat: Supabase conexion configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module'; 
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), 
+    
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true, 
+    }),
+
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
