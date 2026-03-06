1c0e4de29f71507d9349f5b98da1dc3951c986c0
Olga Cantalejo <2004olgacg@gmail.com>
2026-03-06 17:16:48 +0100

    fix: errors in ci_frontend and vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    !process.env.VITEST && tailwindcss(),
-  ].filter(Boolean), 
-
+  plugins: [react()], 
   build: {
     rollupOptions: {
       output: {
